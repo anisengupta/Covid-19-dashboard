e39f7e518981ff211d00e2d523d225476413abe0
AniSengupta <aniruddha.sengupta@omd.com>
2021-08-22 20:19:42 +0100

    Changed the cache config to Redis in __init__.py.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 import dash
 import dash_bootstrap_components as dbc
 from flask_caching import Cache
+from utils import config
 
 # Initiate the app
 app = dash.Dash(
@@ -14,11 +15,13 @@ app = dash.Dash(
 app.title = 'Covid 19 data dashboard'
 
 # Set the cache config
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 7200,
+    'CACHE_REDIS_HOST': config.REDISHOST,
+    'CACHE_REDIS_PORT':config.REDISPORT
+    }
+
 
 # Set the cache config
 cache = Cache(app.server,
