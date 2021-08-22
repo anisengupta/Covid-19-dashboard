df2d0940892a2df4451c267de15640c6ea640802
AniSengupta <aniruddha.sengupta@omd.com>
2021-08-22 20:42:55 +0100

    Added print statements in app.py.

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ print('Setting the title')
 title = app_layout.make_navbar_title()
 
 # Set the layout
+print('Setting the layout')
 app_layout_style = {
     "position": "fixed",
     "max-width": "100vw",
@@ -30,34 +31,43 @@ SELECT * FROM {config.table_name}
 """
 
 # Construct the engine url
+print('Constructing the engine url')
 engine_url = dashboard_connector.Postgres(
     username=config.username, password=config.password
 ).construct_engine_url(database=config.database)
 
 # Initiate the connection
+print('Initiating the connection')
 engine = dashboard_connector.Postgres(
     username=config.username, password=config.password
 ).create_engine(engine_url=engine_url)
 
+print('Retrieving the dataframe')
 df = dashboard_connector.Postgres(
     username=config.username, password=config.password
 ).get_data_from_postgres(query=query, engine=engine)
+
+print('Setting to cache')
 cache.set("covid-19-data", df)
 
 # Perform a groupby
+print('Performing a groupby')
 df_groupby = dashboard_connector.DashboardGraphs.group_by_data(
     df=df, group_by_col="location"
 )
 
 # Remove the continents
+print('Removing the continents from the groupby dataframe')
 df_groupby = df_groupby[~df_groupby["location"].isin(config.continents)]
 
 # Make death rate data
+print('Making death rate data')
 df_group_by_death_rate = dashboard_connector.DashboardGraphs.create_death_rate_data(
     group_by_df=df_groupby
 )
 
 # Make a cases by country chart
+print('Making a cases by country chart')
 cases_by_country_chart = app_layout.make_horizontal_bar_chart(
     data=df_groupby.sort_values(by=["total_cases"], ascending=False).head(10),
     x_col="total_cases",
@@ -67,6 +77,7 @@ cases_by_country_chart = app_layout.make_horizontal_bar_chart(
 )
 
 # Make a deaths by country chart
+print('Making a deaths by country chart')
 deaths_by_country_chart = app_layout.make_horizontal_bar_chart(
     data=df_groupby.sort_values(by=["total_deaths"], ascending=False).head(10),
     x_col="total_deaths",
@@ -75,7 +86,8 @@ deaths_by_country_chart = app_layout.make_horizontal_bar_chart(
     desc="Which countries have recorded the most deaths",
 )
 
-# Make a deaths by country chart
+# Make a death rate by country chart
+print('Making a death rate by country chart')
 death_rate_by_country_chart = app_layout.make_horizontal_bar_chart(
     data=df_group_by_death_rate.sort_values(by=["death_rate"], ascending=False).head(
         10
@@ -87,6 +99,7 @@ death_rate_by_country_chart = app_layout.make_horizontal_bar_chart(
 )
 
 # Create a horizontal charts row
+print('Creating a horizontal charts row')
 horizontal_chart_row = html.Div(
     dbc.Row(
         children=[
@@ -113,10 +126,14 @@ horizontal_chart_row = html.Div(
 )
 
 # Make a time series dataframe
+print('Making a time series dataframe')
 df_time_series = dashboard_connector.DashboardGraphs.create_time_series_data(df=df)
+
+print('Setting the time series dataframe to cache')
 cache.set("original-time-series-data", df_time_series)
 
 # Make a time series chart
+print('Making a time series chart')
 time_series_chart = app_layout.make_time_series_chart(
     data=df_time_series,
     x_col="date",
@@ -126,11 +143,14 @@ time_series_chart = app_layout.make_time_series_chart(
 )
 
 # Make time series dropdown options
+print('Making the time series dropdown options')
 options = dashboard_connector.DashboardGraphs.create_dropdown_options(df=df)
 
 # Make the time series dropdown
+print('Creating the time series dropdown')
 dropdown = app_layout.make_time_series_dropdown(options=options)
 
+print('Making a time series row')
 time_series_row = html.Div(
     dbc.Row(
         children=[
@@ -145,10 +165,12 @@ time_series_row = html.Div(
 )
 
 # Make a world map of cases & deaths
+print('Making a world map of cases')
 df_groupby_choropleth_cases = dashboard_connector.DashboardGraphs.create_choropleth_data(
     df=df, col='total_cases'
 )
 
+print('Making a world map of deaths')
 df_groupby_choropleth_deaths = dashboard_connector.DashboardGraphs.create_choropleth_data(
     df=df, col='total_deaths'
 )
@@ -174,6 +196,7 @@ deaths_world_map = app_layout.make_choropleth_map(
 )
 
 # Make a world map row
+print('Making a world map row')
 world_map_row = html.Div(
     dbc.Row(
         children=[
@@ -189,6 +212,7 @@ world_map_row = html.Div(
 )
 
 # Set the layout
+print('Setting the app layout')
 app.layout = html.Div(
     children=[
         title,
@@ -200,8 +224,10 @@ app.layout = html.Div(
 )
 
 # Callbacks
+print('Initiating the app callbacks')
 app_callbacks.register_app_callbacks(app=app)
 
 # Run the app
+print('Running the app')
 if __name__ == '__main__':
     app.run_server(debug=True)
