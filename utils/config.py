9278b52b636332362ea882fea7cdd5ce12f8f89c
AniSengupta <aniruddha.sengupta@omd.com>
2021-08-24 11:32:44 +0100

    Removed the password from the config.py.

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 # Contains all the configurations needed for the Dash app and overall processes
 navbar_logo_url = 'https://www.spi.ox.ac.uk/sites/default/files/styles/mt_image_medium/public/spi/images/article/our_world_in_data.jpg?itok=sXt5oJje'
 username = "postgres"
-password = "Uchihazenzo1994"
+password = "password"
 database = "covid_19"
 table_name = "covid_19_data"
 columns = ["date", "country_code", "location", "geo_id", "total_cases", "total_deaths", "new_cases", "new_deaths"]
